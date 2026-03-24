77aa6417bc9be833e70ec85d825068befab9e4db
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-03-24 16:46:28 +0100

    Add DTOs

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsNumber()
+  @Min(0)
+  stock: number;
+}
