ec84110add4e1e9c0aa0437ad65813a27ce9c463
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-03-25 10:11:21 +0100

    Update products controller using new DTOs

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,40 +9,41 @@ import {
   Post,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductDto } from './create-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  // GET /products
+  // GET /products - public
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
 
-  // GET /products/:id
+  // GET /products/:id - public
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
-  // POST /products
+  // POST /products - protected
   @Post()
-  create(@Body() productData: CreateProductDto) {
-    return this.productsService.create(productData);
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
   }
 
-  // PATCH /products/:id
+  // PATCH /products/:id - protected
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
-    @Body() productData: Partial<CreateProductDto>,
+    @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productsService.update(id, productData);
+    return this.productsService.update(id, updateProductDto);
   }
 
-  // DELETE /products/:id
+  // DELETE /products/:id - protected
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
