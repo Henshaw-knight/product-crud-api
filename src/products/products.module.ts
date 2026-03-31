943c7cdccd92313ba31a60a86ca5b2e88b4973a8
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-03-31 14:00:50 +0100

    Fix import issue on Products module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -7,8 +7,12 @@ import {
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { AuthMiddleware } from 'src/common/middleware/auth/auth.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product]), UsersModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
