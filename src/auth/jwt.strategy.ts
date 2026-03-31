68631ad40f229d07f92530039c0b56aef31312a0
Henshaw J. Essien <josephhensh2000@gmail.com>
2026-03-31 09:31:26 +0100

    Add JWT auth  guard and JWT strategy for validating tokens

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+export interface JwtPayload {
+  sub: number;
+  email: string;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get<string>('JWT_SECRET')!,
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    // return value is attached to req.user
+    return { id: payload.sub, email: payload.email };
+  }
+}
